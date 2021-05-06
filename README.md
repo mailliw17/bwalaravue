bd63aeeccc9ae77f03a332490a348ab805485347
mailliw17 <williamwirahardi@gmail.com>
2021-05-06 16:23:39 +0700

    upload frontend vuejs

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # bwalaravue
-Online Store App - Build With VueJS and Laravel 6.xx
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <Header />
+    <Hero/>
+    <Women/>
+    <Insta/>
+    <PartnerLogo/>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from '@/components/Header.vue'
+import Hero from '@/components/Hero.vue'
+import Women from '@/components/Women.vue'
+import Insta from '@/components/Insta.vue'
+import PartnerLogo from '@/components/PartnerLogo.vue'
+import Footer from '@/components/Footer.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Header,
+    Hero,
+    Women,
+    Insta,
+    PartnerLogo,
+    Footer
+  }
+}
+</script>
